0a3badd1dbcbe5f399c38b8c4f341ae001ec3e99
aeonyuonmiller <aym1@mail.com>
2022-04-23 14:32:09 +0200

    added content

--- pages/about.jsx
+++ pages/about.jsx
@@ -5,10 +5,14 @@ const about = () => {
     return (
     <>
         <Layout>
-                <div>about me</div>
-                <div>2</div>
-                <div>three</div>
-                <div>four</div>
+          <h2>Andreas Y. Müller</h2>
+          <div>
+            <p>I started to learn photoshop at the age of eleven by being inspired by winamp skins back in the nineties.</p>
+            <p>I worked for large-scale international and national companies. Ranging from agencies, insurance, banking, automotive, travel, pharma and fashion.</p>
+            <p>I enjoy helping multifunctional teams, high-fidelity prototyping, finding out about  possibilities with code and working from strategy up until launch.</p>
+          </div>
+          <h2>Clients</h2>
+          <div>Lufthansa, Land Rover, Jaguar, Lotto, Sparkasse, Abro</div>
         </Layout>
     </>
   )
--- pages/components/Nav.jsx
+++ pages/components/Nav.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import Link from 'next/link'
 import Logo from "./Logo"
+import Lottie from 'react-lottie';
+import animationData from '../../public/white-logo.json';
+
+    const defaultOptions = {
+        loop: false,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice"
+        }
+    };
 
 const Nav = () => {
+    
     return (
         <nav>
-            <Logo color="#fff" width="90" />
+            {/* <Logo color="#fff" width="90" /> */}
+            <Lottie options={defaultOptions} width={90} height={34} />
             <Link href="/">Home</Link>
             <Link href="/about">About</Link>
             <Link href="/work">Work</Link>
