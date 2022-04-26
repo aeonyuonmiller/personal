f7d38186d3b7e97f335df7b665d7732bf2c4629f
aeonyuonmiller <aym1@mail.com>
2022-04-26 15:37:29 +0200

    added work page

---
+++ pages/work.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from './components/Layout'
+
+const work = () => {
+  return (
+      <>
+        <Layout>
+            work
+        </Layout>
+      </>
+  )
+}
+
+export default work
