fda64b1e2ca0bf18b69a66f5aff1187a6353e403
aeonyuonmiller <aym1@mail.com>
2023-01-06 02:54:51 +0100

    added images

---
+++ pages/components/Thumbnail.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { motion as m } from "framer-motion"
+
+const Thumbnail = ({ title, description, motive }) => {
+    const zoom = {
+        hidden: { scale: 1.3, rotateX: 15, transition:{ type:"ease", duration: .5 }},
+        show: { scale: 1, rotateX: 0, transition:{ type:"ease", duration: 1 }}
+    }
+    
+  return (
+    <m.div exit={{ opacity: 0 }} className="thumbnail-container">
+        <m.img variants={zoom} initial="hidden" whileInView="show" viewport={{ amount: 0.4 }} exit="hidden" src={motive} />
+        <m.h3 initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }}>{title}</m.h3>
+        <h5>{description}</h5>
+        <span className="arrow">•</span>
+    </m.div>
+  )
+}
+
+export default Thumbnail
