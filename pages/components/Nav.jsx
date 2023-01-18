caa3a6f9f858f83ebb2acd69c5a3436af09e298a
aeonyuonmiller <aym1@mail.com>
2023-01-18 12:23:52 +0100

    fixed portrait

--- pages/components/Nav.jsx
+++ pages/components/Nav.jsx
@@ -4,12 +4,12 @@ import { motion as m } from "framer-motion"
 
 const Nav = ({title, gototitle, url}) => {
     return (
-    <nav>
+    <m.nav initial={{opacity:0}} animate={{opacity:1}}>
         <Link href={`/${url}`}>
             <m.span>{title}</m.span>
-            <m.span exit={{ opacity: 0 }}>{gototitle}</m.span>
+            <m.span exit={{ opacity: 0, transition:{ duration: .6 }}}>{gototitle}</m.span>
         </Link>
-    </nav>
+    </m.nav>
   )
 }
 
