d207c77fb3733a69b312f94db6dd8fe2e7429cc5
aeonyuonmiller <aym1@mail.com>
2023-01-20 18:50:52 +0100

    added animated SVG logo

---
+++ pages/components/LogoIcon.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import { motion as m } from "framer-motion"
+
+function Icon() {
+  
+const group = {
+  show: { opacity: 1, transition:{ staggerChildren: 0.15, } },
+  hide: { opacity: 0 },
+}
+
+const item = {
+  show: { x: 0, transition: { ease: [.71, .01, .42, .99], duration: 2, }},
+  hide: { x: "100%" },
+}
+  return (
+    <m.svg
+      variants={group}
+      initial="hide"
+      animate="show"
+      xmlns="http://www.w3.org/2000/svg"
+      width="100"
+      fill="none"
+      viewBox="0 0 742 300"
+      >
+        <m.g variants={item}>
+        <rect
+            width="80"
+            height="80"
+            x="0.928"
+            y="83.995"
+            fill="#fff"
+            rx="40"   
+        ></rect></m.g>
+        <m.g variants={item}>
+        <rect     
+            width="160"
+            height="80"
+            x="86.183"
+            y="0.74"
+            fill="#fff"
+            rx="40"
+            transform="rotate(45 86.183 .74)"
+              ></rect></m.g>
+        <m.g variants={item}>
+        <rect
+            width="160"
+            height="80"
+            x="212.497"
+            y="2.995"
+            fill="#fff"
+            rx="40"
+            transform="rotate(45 212.497 2.995)"
+              ></rect></m.g>
+        <m.g variants={item}>
+        <rect
+            width="160"
+            height="80"
+            fill="#fff"
+            rx="40"
+            transform="scale(-1 1) rotate(45 -283 -383.293)"
+        ></rect></m.g>
+        <m.g variants={item}>
+        <rect
+            width="160"
+            height="80"
+            x="402.497"
+            y="11.995"
+            fill="#fff"
+            rx="40"
+            transform="rotate(45 402.497 11.995)"
+        ></rect></m.g>
+        <m.g variants={item}>
+        <rect
+            width="160"
+            height="80"
+            x="515.69"
+            y="12.188"
+            fill="#fff"
+            rx="40"
+            transform="rotate(45 515.69 12.188)"
+        ></rect></m.g>
+        <m.g variants={item}>
+        <rect
+            width="160"
+            height="80"
+            x="628.759"
+            y="12.257"
+            fill="#fff"
+            rx="40"
+            transform="rotate(45 628.759 12.257)"
+        ></rect></m.g>
+    </m.svg>
+  );
+}
+
+export default Icon;
