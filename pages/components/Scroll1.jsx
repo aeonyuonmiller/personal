13ed44698f8d8d7c259e6f97f0b302178c46c8b2
aeonyuonmiller <aym1@mail.com>
2023-01-23 00:40:12 +0100

    updated scroll-linked animation

--- pages/components/Scroll1.jsx
+++ pages/components/Scroll1.jsx
@@ -2,10 +2,12 @@ import { motion as m, useScroll, useTransform } from "framer-motion"
 
 const Scroll1 = ({children}) => {
     const { scrollYProgress } = useScroll();
-    const y = useTransform(scrollYProgress, [0, .5], [1, -2]);
+    const trans = useTransform(scrollYProgress, [0, .5], [1, -2]);
+    const y = useTransform(scrollYProgress, [0, .5], [1, -60]);
+
 
   return (
-    <m.div style={{ opacity: y }}>{children}</m.div>
+    <m.div style={{ opacity: trans, y: y }}>{children}</m.div>
   )
 }
 
