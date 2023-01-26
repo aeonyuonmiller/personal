b5914bdb964dceb5cc6e52eef8963e3bf9665100
aeonyuonmiller <aym1@mail.com>
2023-01-26 01:01:58 +0100

    copy

--- pages/about.jsx
+++ pages/about.jsx
@@ -50,10 +50,10 @@ const about = ({ page }) => {
             animate={{ scale: 1, opacity: 1, y: 0, transition:{ ease: [.64, .62, .23, .99], duration: .8, delay: .4 } }}
             exit={{ opacity: 0, y: 5, transition: { ease: "linear", duration: .4, delay: .3 } }}>
             {/* <PrismicRichText field={data.copy} /> */}
-            <p className='intro'>My name is Andreas Y. Müller or aeonyuonmiller and I am a UI designer with over 10 years of experience.</p>
-            <Fade>I work with Figma for conception and design and with code – specifically with React (Next.js), Framer-Motion, Prismic and Snipcart.</Fade>
+            <p className='intro'>My name is Andreas Y. Müller or aeonyuonmiller and I am a <strong>UI designer</strong> with over 10 years of experience.</p>
+            <Fade>I work with <strong>Figma</strong> for conception and design and with code – specifically with <strong>React</strong> (Next.js), <strong>Framer-Motion</strong>, <strong>Prismic</strong> and <strong>Snipcart</strong>.</Fade>
             <Fade>I have a passion for creating smooth interactive products and building consistent design-systems that provide a seamless user experience. I pride myself on my ability to translate complex design concepts into intuitive and visually appealing interfaces.</Fade>
-            <Fade>In my past work, I have successfully collaborated with cross-functional teams to deliver high-quality products. Through my experience in agencies i am confident in my ability to adapt to new challenges and thrive in a fast-paced environment.</Fade>
+            <Fade>In the past, I have successfully collaborated with cross-functional teams to deliver high-quality products. Through my experience in agencies i am confident in my ability to adapt to new challenges and thrive in a fast-paced environment.</Fade>
             <Fade>I am excited to bring my skills and experience to new projects and help bring visions to life.</Fade>
             <Signature />
           </m.div>
