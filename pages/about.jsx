bc92322347a247e8f018dcb57d0df399ee1344b3
aeonyuonmiller <aym1@mail.com>
2023-01-27 01:25:28 +0100

    styling

--- pages/about.jsx
+++ pages/about.jsx
@@ -38,7 +38,6 @@ const about = ({ page }) => {
           exit={{ scale: 1, opacity: 0, transition: { ease: [.63, 0, .17, .99], duration: .8, delay: .2 } }}>
           {/* <Image width="900" height="600" alt="Portrait shot of Aeon Yuon Miller" src="/portrait.png" /> */}
           <img loading="lazy" role="presentation" alt="Portrait shot of Aeon Yuon Miller" src="/portrait.png" />
-
         </m.div>
         
 
