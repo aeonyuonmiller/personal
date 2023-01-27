6c130cba8c69e4d5454435a6e1b63d47f3e5e645
aeonyuonmiller <aym1@mail.com>
2023-01-27 19:13:50 +0100

    updates

--- pages/components/Mp3Player.jsx
+++ pages/components/Mp3Player.jsx
@@ -43,7 +43,67 @@ const MP3Player = () => {
                 value={currentTime}
                 onChange={handleSeek}
             />
-            
+            <style jsx>{`
+            /*********** Baseline, reset styles ***********/
+            input[type="range"] {
+            -webkit-appearance: none;
+            appearance: none;
+            background: transparent;
+            cursor: pointer;
+            width: 100%;
+            }
+
+            /* Removes default focus */
+            input[type="range"]:focus {
+            outline: none;
+            }
+
+            /******** Chrome, Safari, Opera and Edge Chromium styles ********/
+            /* slider track */
+            input[type="range"]::-webkit-slider-runnable-track {
+            background-color: #a8e9ff;
+            border-radius: 1.5rem;
+            height: 1.5rem;
+            }
+
+            /* slider thumb */
+            input[type="range"]::-webkit-slider-thumb {
+            -webkit-appearance: none; /* Override default look */
+            appearance: none;
+            margin-top: -4px; /* Centers thumb on the track */
+            background-color: #000000;
+            border-radius: 1rem;
+            height: 2rem;
+            width: 2rem;
+            }
+
+            input[type="range"]:focus::-webkit-slider-thumb {
+            outline: 3px solid #000000;
+            outline-offset: 0.125rem;
+            }
+
+            /*********** Firefox styles ***********/
+            /* slider track */
+            input[type="range"]::-moz-range-track {
+            background-color: #a8e9ff;
+            border-radius: 1.5rem;
+            height: 1.5rem;
+            }
+
+            /* slider thumb */
+            input[type="range"]::-moz-range-thumb {
+            background-color: #000000;
+            border: none; /*Removes extra border that FF applies*/
+            border-radius: 1rem;
+            height: 2rem;
+            width: 2rem;
+            }
+
+            input[type="range"]:focus::-moz-range-thumb{
+            outline: 3px solid #000000;
+            outline-offset: 0.125rem;
+            }
+            `}</style>
         </div>
     )
 }
