a7cfbf187c6beb24aca000ea8bb993e76089f055
aeonyuonmiller <aym1@mail.com>
2023-01-28 21:11:54 +0100

    cleanup

--- pages/components/Thumbnail.jsx
+++ pages/components/Thumbnail.jsx
@@ -2,20 +2,21 @@ import Link from "next/link"
 import { motion as m, AnimatePresence } from "framer-motion"
 import ArrowIcon from "./ArrowIcon"
 
-const Thumbnail = ({ title, description, url, src }) => {
+const Thumbnail = ({ title = "", description = "", url = "", src = "" }) => {
 
     const zoom = {
-      hide: { scale: 1.15, opacity:null, transition:{ ease:[.79,.41,.07,.99], duration: .6 }},
+      hide: { scale: 1.15, opacity: null, transition:{ ease:[.79,.41,.07,.99], duration: .6 }},
       show: { scale: 1, opacity:1, transition: { ease: [.79, .41, .07, .99], duration: 1 }}
     }
   
   return (
     <>
       <AnimatePresence mode="wait">
-        <Link prefetch href={url ?? ''}>
+        {/* <Link prefetch href={url ?? ''}> */}
+        <Link prefetch href={url}>
           <m.div variants={zoom} initial="hide" animate="show" exit="hide" className="thumbnail-container">
             <m.img role="presentation" variants={zoom} initial="hide" whileInView="show" viewport={{ amount: 0.5, once: true }} src={src} />
-            <m.h3 initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} exit="hide">{title}</m.h3>
+            <m.h3 initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: 20 }} transition={{ease: "easeOut", duration: .4}}>{title}</m.h3>
             <h5>{description}</h5>
             <span className="arrow"><ArrowIcon /></span>
           </m.div>
