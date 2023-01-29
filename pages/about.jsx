d608cf6fea54b700fe66c24e2c8ac86c4a0dd43a
aeonyuonmiller <aym1@mail.com>
2023-01-29 04:34:31 +0100

    update

--- pages/about.jsx
+++ pages/about.jsx
@@ -51,7 +51,7 @@ const about = ({ page }) => {
             {/* <PrismicRichText field={data.copy} /> */}
             <p className='intro'>My name is Andreas Y. Müller or aeonyuonmiller and I am a UI designer with over 10 years of experience.</p>
             <Fade>I work with <strong>Figma</strong> for conception and design and with code – specifically with <strong>React</strong> (Next.js), <strong>Framer-Motion</strong>, <strong>Prismic</strong> and <strong>Snipcart</strong>.</Fade>
-            <Fade>Currently I'm getting into ThreeJS (React-Three-Fiber & Drei).</Fade>
+            <Fade>Currently getting into ThreeJS (R3F & Drei).</Fade>
             <Fade>I have a passion for creating smooth interactive products and building consistent design-systems that provide a seamless user experience. I pride myself on my ability to translate complex design concepts into intuitive and visually appealing interfaces.</Fade>
             <Fade>In the past, I have successfully collaborated with cross-functional teams to deliver high-quality products. Through my experience in agencies i am confident in my ability to adapt to new challenges and thrive in a fast-paced environment.</Fade>
             <Fade>I am excited to bring my skills and experience to new projects and help bring visions to life.</Fade>
