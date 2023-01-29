b6c44487363973c3562a7221e495556db2869a1d
aeonyuonmiller <aym1@mail.com>
2023-01-29 04:17:49 +0100

    updated Image

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { motion as m } from "framer-motion"
 import Head from 'next/head'
 import Image from 'next/image'
-import portrait from "../public/portrait.png"
 
 // components
 import Nav from "./components/Nav"
@@ -37,9 +36,8 @@ const about = ({ page }) => {
           initial={{ scale: 1.05, opacity: 0 }}
           animate={{ scale: 1.025, opacity: 1, y:0, transition: { ease: [.64, .62, .23, .99], duration: .8 } }}
           exit={{ scale: 1, opacity: 0, transition: { ease: [.63, 0, .17, .99], duration: .8, delay: .2 } }}>
-          {/* <Image width="900" height="600" alt="Portrait shot of Aeon Yuon Miller" src="/portrait.png" /> */}
+          <Image priority width="600" height="700" alt="Portrait shot of Aeon Yuon Miller" src="/portrait.png" />
           {/* <img loading="lazy" role="presentation" alt="Portrait shot of Aeon Yuon Miller" src="/portrait.png" /> */}
-          <Image fill alt="Picture of Author" src={portrait} />
         </m.div>
         
 
