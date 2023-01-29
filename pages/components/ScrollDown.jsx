ff1171acc9a521fe4828f22d90227c5299488d54
aeonyuonmiller <aym1@mail.com>
2023-01-29 16:25:08 +0100

    update

--- pages/components/ScrollDown.jsx
+++ pages/components/ScrollDown.jsx
@@ -5,7 +5,9 @@ import ArrowIcon from "./ArrowIcon"
 
 const ScrollDown = () => {
   return (
-      <m.div initial={{y:null}} animate={{y:[0,10,0], transition:{ repeat: Infinity, duration:1 }}} className='scroll-down'><ArrowIcon /></m.div>
+      <m.div initial={{ y: null }} animate={{ y: [0, 10, 0], transition: { repeat: Infinity, duration: 1 } }} className='scroll-down'>
+        <ArrowIcon />
+      </m.div>
   )
 }
 
