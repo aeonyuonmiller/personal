73cd5279284856345c45535e725a6b9aa094496a
aeonyuonmiller <aym1@mail.com>
2023-01-29 02:46:39 +0100

    smoothened transitions

--- pages/components/Close.jsx
+++ pages/components/Close.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { motion as m } from "framer-motion"
+import Link from 'next/link'
 
 const Close = () => {
-  return (
-      <m.div className='close'>Close</m.div>
+    return (
+        <Link href="/">
+            <m.div initial={{scale:0}} animate={{scale:1, transition:{duration: .6, delay: .8}}} exit={{scale:0, rotate: 90, y: -50, transition:{duration: .6, delay: .2}}} className='close'>Close</m.div>
+        </Link>
       )
 }
 
