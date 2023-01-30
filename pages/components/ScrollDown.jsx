f376ace402c351aab30106a5ad153330b8b17913
aeonyuonmiller <aym1@mail.com>
2023-01-30 01:36:22 +0100

    changed copy

--- pages/components/ScrollDown.jsx
+++ pages/components/ScrollDown.jsx
@@ -7,8 +7,8 @@ const ScrollDown = () => {
   return (
     <m.div initial={{ opacity:0 }} animate={{opacity:1, transition:{duration: 1, delay:2}}} exit={{opacity:0}}>
       <m.div initial={{ y: 0 }} whileInView={{ y: [0, 10, 0], transition: { repeat: Infinity, duration: 1 } }} className='scroll-down'>
+        Scroll
         <ArrowIcon />
-        Scroll down
       </m.div>
     </m.div>
   )
