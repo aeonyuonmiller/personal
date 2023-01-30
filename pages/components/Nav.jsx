a2c2d801692d62ae524700b31c61278d861cf5b7
aeonyuonmiller <aym1@mail.com>
2023-01-30 23:48:13 +0100

    readable code

--- pages/components/Nav.jsx
+++ pages/components/Nav.jsx
@@ -6,7 +6,13 @@ const Nav = ({ title, gototitle, url }) => {
     return (
     <m.nav>
         {/* <Link href={`/${url}`}> */}
-            / <m.span initial={{opacity:0, y:-10}} animate={{opacity:1, y:0, transition:{duration:.2, ease: [1,-0.01,0,.99]}}} exit={{opacity:0, y: 10, transition:{duration:.2, ease: "linear"}}}>{title}</m.span>
+            /
+            <m.span
+                initial={{ opacity: 0, y: -10 }}
+                animate={{ opacity: 1, y: 0, transition: { duration: .2, ease: [1, -0.01, 0, .99] } }}
+                exit={{ opacity: 0, y: 10, transition: { duration: .2, ease: "linear" } }}>
+                {title}
+            </m.span>
             {/* <m.span exit={{ opacity: 0, transition: { duration: .6 }}}>{gototitle}</m.span> */}
         {/* </Link> */}
     </m.nav>
