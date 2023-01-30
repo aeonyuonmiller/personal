1fdef931d2e5b2889650ba0ad3a7e6f18abbacff
aeonyuonmiller <aym1@mail.com>
2023-01-30 16:32:56 +0100

    removed layout shift

--- pages/components/Nav.jsx
+++ pages/components/Nav.jsx
@@ -4,7 +4,7 @@ import { motion as m } from "framer-motion"
 const Nav = ({ title, gototitle, url }) => {
 
     return (
-    <m.nav layout>
+    <m.nav>
         {/* <Link href={`/${url}`}> */}
             / <m.span initial={{opacity:0, y:10}} animate={{opacity:1, y:0, transition:{duration:.4, ease: [1,-0.01,0,.99]}}} exit={{opacity:0, transition:{duration:.2, ease: "linear"}}}>{title}</m.span>
             {/* <m.span exit={{ opacity: 0, transition: { duration: .6 }}}>{gototitle}</m.span> */}
