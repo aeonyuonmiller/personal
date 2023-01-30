5459467a3d0e7ded889307b2a00dff6e49e536eb
aeonyuonmiller <aym1@mail.com>
2023-01-30 15:35:09 +0100

    update

--- pages/components/Scroll1.jsx
+++ pages/components/Scroll1.jsx
@@ -2,12 +2,12 @@ import { motion as m, useScroll, useTransform } from "framer-motion"
 
 const Scroll1 = ({children}) => {
     const { scrollYProgress } = useScroll();
-    const trans = useTransform(scrollYProgress, [0, .5], [1, -2]);
-    const y = useTransform(scrollYProgress, [0, .5], [1, -60]);
+    const trans = useTransform(scrollYProgress, [0, 0.2, 1], [1, 1, -2]);
+    const y = useTransform(scrollYProgress, [0, .5], [1, -50]);
 
 
   return (
-    <m.div style={{ opacity: trans, y: y }}>{children}</m.div>
+    <m.div style={{ opacity:trans, y:y }}>{children}</m.div>
   )
 }
 
