dd8314e4202ccb8597a8d6f25c9011cbfd507a66
aeonyuonmiller <aym1@mail.com>
2023-02-01 13:32:59 +0100

    updated behavior

--- pages/components/Scroll1.jsx
+++ pages/components/Scroll1.jsx
@@ -2,7 +2,7 @@ import { motion as m, useScroll, useTransform } from "framer-motion"
 
 const Scroll1 = ({children}) => {
     const { scrollYProgress } = useScroll();
-    const trans = useTransform(scrollYProgress, [0.2, 1], [1, -2]);
+    const trans = useTransform(scrollYProgress, [0, .5], [1, -2]);
     const y = useTransform(scrollYProgress, [0, .5], [1, -50]);
 
 
