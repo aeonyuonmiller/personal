83d53e325ec0d57d005c6d9913feb8abdac9ccbc
aeonyuonmiller <aym1@mail.com>
2023-02-02 23:52:11 +0100

    mobile update

--- pages/components/Copyright.jsx
+++ pages/components/Copyright.jsx
@@ -16,9 +16,9 @@ const Copyright = () => {
     <m.div variants={parent} initial="initial" animate="animate" className="copyright">
       <m.span variants={child}>&copy;&nbsp;</m.span>
       <m.span>{new Date().getFullYear()}</m.span>
-      <m.div variants={child}><Link tabIndex={5} scroll={false} href="/imprint">Imprint</Link></m.div>
-      <m.div variants={child}><Link tabIndex={3} scroll={false} href="/about">About</Link></m.div>
-      <m.div variants={child}><a target="_blank" rel="noreferrer" tabIndex={4} href="https://www.linkedin.com/in/andreas-y-m%C3%BCller/">Linkedin<ArrowIconPath/></a></m.div>
+      <m.span className="move" variants={child}><Link tabIndex={5} scroll={false} href="/imprint">Imprint</Link></m.span>
+      <m.span className="move" variants={child}><Link tabIndex={3} scroll={false} href="/about">About</Link></m.span>
+      <m.span variants={child}><a target="_blank" rel="noreferrer" tabIndex={4} href="https://www.linkedin.com/in/andreas-y-m%C3%BCller/">Linkedin<ArrowIconPath/></a></m.span>
     </m.div>
   )
 }
