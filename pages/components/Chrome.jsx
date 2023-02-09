55633da70d2085aff66fca120a31dec517054016
aeonyuonmiller <aym1@mail.com>
2023-02-09 01:26:09 +0100

    does it build?

--- pages/components/Chrome.jsx
+++ pages/components/Chrome.jsx
@@ -3,7 +3,7 @@ import { useGLTF, useAnimations } from "@react-three/drei";
 
 export function Chrome(props) {
   const group = useRef();
-  const { nodes, materials, animations } = useGLTF("/aym.gltf");
+  const { nodes, materials, animations } = useGLTF("uaym.gltf");
   const { actions } = useAnimations(animations, group);
   return (
     <group ref={group} {...props} dispose={null}>
