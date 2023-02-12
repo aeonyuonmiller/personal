1fd140dfca1fe7ff1e8584558931b113ce3f14bb
aeonyuonmiller <aym1@mail.com>
2023-02-12 18:04:24 +0100

    still correcting bigimage component

--- pages/components/BigImage.jsx
+++ pages/components/BigImage.jsx
@@ -1,28 +1,35 @@
-import { m, useScroll, useSpring } from "framer-motion"
+import { m } from "framer-motion"
 import Image from "next/image"
 
 const BigImage = ({ src = "", alt = "", marginBottom = "" }) => {
-  const { scrollYProgress } = useScroll()
-  const scaleX = useSpring(scrollYProgress)
+  // const { scrollYProgress } = useScroll()
+  // const scaleX = useSpring(scrollYProgress)
 
   return (
   
     <div className="image-container" style={{ marginBottom }}>
-      <m.span style={{ scaleX }} initial={{ opacity:0 }} whileInView={{ opacity: 1 }} viewport={{ once: true }}><Image fill={true} src={src} alt={alt} /></m.span>
+      <m.span initial={{ scale: 1.05 }} whileInView={{ scale:1 }}>
+        <Image width={1600} height={700} quality={90} src={src} alt={alt} />
+      </m.span>
           
       <style jsx>{`
         .image-container {
           position: relative;
           grid-column: 1 / 4;
           width: 100%;
-          min-height: 50vh;
-          aspect-ratio: 16 / 9;
+          /* min-height: 50vh; */
+          height: auto;
+          /* aspect-ratio: 16 / 9; */
           overflow: hidden;
+          object-fit: contain;
+        }
+        .image-container span{
+          width: 100%;
         }
         .image-container img{ 
-          position: relative;
-          width: 100%;
-          object-fit: cover;
+          position: absolute;
+          width: auto;
+          height: 100%;
         }
       `}</style>
 
