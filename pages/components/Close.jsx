eafbb384abcea514e882aaf9d3f43c26e1e4bd19
aeonyuonmiller <aym1@mail.com>
2023-02-12 18:05:43 +0100

    slowed down whilehover

--- pages/components/Close.jsx
+++ pages/components/Close.jsx
@@ -9,7 +9,7 @@ const Close = () => {
             <m.div
                 initial={{ scale: 0, rotate: -90 }}
                 animate={{ scale: 1, rotate: 0, transition: { type: "spring", duration: .6 }}}
-                whileHover={{ scale: .8, rotate: 90, transition: { type: "spring", duration: .2 }}}
+                whileHover={{ scale: .8, rotate: 90, transition: { type: "spring", duration: .4 }}}
                 exit={{ scale: 0, rotate: 225, y: -50, transition: { duration: .6, delay: .2 }}}
                 className='close'
             >
