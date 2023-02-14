27f86423029e0d25217656a0b4269884e37fd5aa
aeonyuonmiller <aym1@mail.com>
2023-02-14 11:47:56 +0100

    updated .close

--- pages/components/Close.jsx
+++ pages/components/Close.jsx
@@ -10,7 +10,7 @@ const Close = () => {
                 initial={{ scale: 0, rotate: -90 }}
                 animate={{ scale: 1, rotate: 0, transition: { type: "spring", duration: .6 }}}
                 whileHover={{ scale: .8, rotate: 90, transition: { type: "spring", duration: .4 }}}
-                exit={{ scale: 0, rotate: 225, y: -50, transition: { duration: .6, delay: .2 }}}
+                exit={{ scale: 0, rotate: 225, transition: { duration: .6, delay: .2 }}}
                 className='close'
             >
                 <CloseIcon />
