c6a1543eee4c2e3de00ea633629a059068109610
aeonyuonmiller <aym1@mail.com>
2023-02-23 05:01:52 +0100

    removed unused

--- pages/components/Capsule.jsx
+++ pages/components/Capsule.jsx
@@ -1,8 +1,8 @@
 import React, { useRef } from "react";
-import { Vector3 } from 'three'
-import { useGLTF, useAnimations, MeshDistortMaterial } from "@react-three/drei";
+import { useGLTF, useAnimations } from "@react-three/drei";
 import { useFrame, useThree } from '@react-three/fiber'
 import dynamic from "next/dynamic";
+import { motion } from "framer-motion-3d"
 
 export function Capsule(props) {
   const group = useRef();
@@ -12,24 +12,23 @@ export function Capsule(props) {
   const { viewport } = useThree()
   // viewport = canvas in 3d units (meters)
   const ref = useRef()
-  const vec = new Vector3()
 
   useFrame(({ mouse }) => {
     const x = (mouse.x * viewport.width) / 2
     const y = (mouse.y * viewport.height) / 2
     ref.current.position.set(x/60, y/60, 0)
-    ref.current.rotation.set(-y/60, x/60, 0)
+    ref.current.rotation.set(-y / 60, x / 60, 0)
   })
 
   return (
     <group ref={group} {...props} dispose={null}>
-        <mesh
-          ref={ref}
-          name="liquid"
-          geometry={nodes.liquid.geometry}
-          material={materials.CrackedMetal}
-          position={[0, 0, 0]}
-          rotation={[0, 0, 50]}
+      <motion.mesh
+        ref={ref}
+        name="liquid"
+        geometry={nodes.liquid.geometry}
+        material={materials.CrackedMetal}
+        position={[null, null, null]}
+        rotation={[null, null, null]}
       />
     </group>
   );
