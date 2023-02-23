85252285ef8ffbb9c4328e19ad0d071e860797c3
aeonyuonmiller <aym1@mail.com>
2023-02-23 05:01:42 +0100

    added loader

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Nav from "./components/Nav"
 import { m } from "framer-motion"
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Preload, Stage } from '@react-three/drei'
+import { OrbitControls, Preload, Stage, Loader } from '@react-three/drei'
 // import { Chrome } from './components/Chrome'
 import { Capsule } from './components/Capsule'
 
@@ -50,7 +50,7 @@ export default function Home() {
 
       <m.div className='scrollcontainer' ref={scrollDiv}
         initial={{ y: "50%", opacity: 0 }}
-        animate={{ y: 0, opacity: 1, transition:{ ease: [.64, .62, .23, .99], duration: .8, delay: 1 }}}
+        animate={{ y: 0, opacity: 1, transition:{ ease: [.9, .62, .23, .99], duration: .8, delay: 1 }}}
         exit={{ y: "-50%", opacity: [1,0], transition: { ease: [.9, 0, .17, .99], duration: .8 }}}
       >    
         {children}
@@ -62,16 +62,17 @@ export default function Home() {
 
       <m.div drag className='chrome' initial={{opacity:0, y:20, x:"10%"}} animate={{opacity:1, y:0, x:0, transition: {duration:1}}} exit={{opacity:0, y:20, rotate: 16, scale: 1.6, transition: {duration:.4}}}>
         <Canvas frameloop="demand" camera={{ position: [-10, 180, 0], fov: 50 }}>
-            <Suspense fallback={<span>loading model...</span>}/>
-            {/* <color attach="background" args={[0,0,0,0]} /> */}
-              <ambientLight intensity={0.3} />
-              <pointLight position={[10,30,20]} color="blue" intensity={10} />
-              <Stage controls={ref} preset="soft" intensity={0.9}  contactShadow={false} shadows={false} environment="warehouse">
-                <Capsule />
-              </Stage>
-              <OrbitControls enableZoom={false} enablePan={false} enableRotate={true} ref={ref} />
-            <Preload all />
+          {/* <color attach="background" args={[0,0,0,0]} /> */}
+          <ambientLight intensity={0.3} />
+          <pointLight position={[10,30,20]} color="blue" intensity={10} />
+          <Stage controls={ref} preset="soft" intensity={0.9}  contactShadow={false} shadows={false} environment="warehouse">
+            <Suspense fallback={<span>loading model...</span>} />
+            <Capsule />
+          </Stage>
+          <OrbitControls enableZoom={false} enablePan={false} enableRotate={true} ref={ref} />
+          <Preload all />
         </Canvas>
+        <Loader dataInterpolation={(p) => `Loading ${p.toFixed(2)}%`} />
       </m.div>
     </>
   )
