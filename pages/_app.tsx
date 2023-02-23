f2e9e0012a5e2185ff56136d7db9c6c7a5a65fe0
aeonyuonmiller <aym1@mail.com>
2023-02-23 05:01:25 +0100

    removed unused

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,18 @@
 import type { AppProps } from 'next/app'
-// import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 import { Analytics } from '@vercel/analytics/react';
 import '../styles/globals.css'
 import { LazyMotion, domAnimation, AnimatePresence, MotionConfig } from "framer-motion";
 
 // components
 import Logo from "./components/Logo"
-// import Menu from "./components/Menu"
 import Connect from "./components/Connect"
 import Copyright from "./components/Copyright"
 
 export default function App({ Component, pageProps, router }: AppProps) {
 
   return <>
-      <MotionConfig reducedMotion="user">
+    <MotionConfig reducedMotion="user">
         <LazyMotion strict features={domAnimation}>
           <AnimatePresence mode="wait">
             <Component {...pageProps} key={router.route} />
